99994ad6b721218c5c330312a9c31375f7f6f10c
abhinav3254 <abhinav3254@gmail.com>
2023-05-31 11:18:13 +0530

    external js

---
+++ JS/learn/myScript.js
@@ -0,0 +1,6 @@
+function myFunction1() {
+    document.getElementById("demo-3").innerHTML = "New Desiging";
+}
+function myFunction2() {
+    document.getElementById("demo-3").innerHTML = "Reversed it";
+}
